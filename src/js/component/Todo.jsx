7dae483dccb62a7cf56b449804ecbcc7ea343966
baguajoe <baguajoe@gmail.com>
2024-08-30 21:07:34 +0000

    apireact

--- src/js/component/Todo.jsx
+++ src/js/component/Todo.jsx
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { fetchTasks, addTasksToApi, deleteTaskFromApi, createUser } from "../todoApi";
 
 //create your first component
 const Todo = () => {
 	const [inputValue, setInputValue] = useState('');
 	const [tasks, setTasks] = useState([]);
 
-	const handleAddTodo = (e) => {
+	useEffect(
+		() => {
+			fetchTasks(setTasks);
+		}, [setTasks]
+	)
+
+	const handleAddTasks = (e) => {
 		if (e.key === "Enter" && inputValue.trim() !== "") {
-			setTasks([...tasks, inputValue.trim()]);
+			const newTask = {
+				id: Date.now(),
+				label: inputValue.trim(), 
+				done: false
+			};
+			setTasks([ ...tasks, newTask ]);
+
+			addTasksToApi(tasks, inputValue, setTasks);
+
 			setInputValue("");
 		}
 	};
 	const handleDeleteTask = (index) => {
-		setTasks(tasks.filter((task, i) => index !== i))
+		const taskId = tasks[index].id;
+		const taskLabel = tasks[index].label;
+		const updatedTasks = tasks.filter((task, i) => index !== i);
+		setTasks(updatedTasks);
+
+		deleteTaskFromApi(taskId, setTasks);
 	};
 
 	return (
 		<div className="container pb-5 px-4 rounded">
 			<h1 className="mt-5 text-center text-white">To Do List</h1>
+			
 			<div className="card task-card mx-auto mt-5 ">
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item">
@@ -27,7 +47,7 @@ const Todo = () => {
 							type="text"
 							onChange={e => setInputValue(e.target.value)}
 							value={inputValue}
-							onKeyDown={handleAddTodo}
+							onKeyDown={handleAddTasks}
 							placeholder="What needs to be done?"
 						/>
 					</li>
@@ -36,22 +56,26 @@ const Todo = () => {
 						<li className="list-group-item no-tasks">No tasks, add a task</li>
 					) : (
 						tasks.map((task, index) => (
-							<li key={index} className="list-group-item">
-								<div className = "list-group-item-tasks">{task}</div>
-								<span className= "x-container" onClick={()=> handleDeleteTask(index)}> 
-								<i className="fa-solid fa-x"></i>
+							<li key={task.id} className="list-group-item">
+								<div className="list-group-item-tasks">{task.label}</div>
+								<span className="x-container" onClick={() => handleDeleteTask(index)}>
+									<i className="fa-solid fa-x"></i>
 								</span>
-							
+
 							</li>
 						))
 					)}
 				</ul>
 				<div className="card-footer text-secondary">
 					{tasks.length} {tasks.length === 1 ? "item" : "items"}
-				</div>	
+				</div>
+			</div>
+			<div className ="buttonGroup mt-4 gap-2 d-flex justify-content-center">
+					<button className ="btn btn-light"type="button" onClick={() => createUser(setTasks)}>Add User To Save Tasks</button>
+					<button className ="btn btn-light"type="button">Delete User & All Tasks</button>
 			</div>
 		</div>
 	);
 };
-
+// delete tasks and delete user
 export default Todo;
--- src/js/todoApi.js
+++ src/js/todoApi.js
@@ -23,7 +23,6 @@ export const createUser = (setTasks) => {
 export const fetchTasks = (setTasks) => {
     fetch(`${API_URL}/users/${username}`, {
         method: "GET",
-        body: JSON.stringify([]),
         headers: {
             "Content-Type": "application/json"
         }
@@ -32,9 +31,52 @@ export const fetchTasks = (setTasks) => {
             if (!resp.ok) {
                 throw new Error("failed to fetch todo list. status: ", resp.status);
             }
-            console.log(resp.ok); // Will be true if the response is successful
-            console.log(resp.status); // The status code=200 or code=400 etc.
-            console.log(resp.text()); // Will try to return the exact result as a string
-            return resp.json(); // (re
+            console.log(resp); 
+            return resp.json(); 
         })
-}
+        .then((data) => {
+            console.log("Todo list from API", data);
+            if (Array.isArray(data.todos)) {
+                setTasks(data.todos)
+            } else {
+                console.error("fetch data is not an array:", data.todos);
+                setTasks([]);
+
+            }
+        }) 
+        .catch((error) => console.error("there has been a problem with your fetch operation:", error));
+};
+  
+export const addTasksToApi = async (tasks, inputValue, setTasks) => {
+    try {
+        const newTask = {
+            "label": inputValue.trim(),
+            "is_done": false
+        }
+        const response = await fetch(`${API_URL}/todos/${username}`, {
+            method: "POST",
+            body: JSON.stringify(newTask),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        
+        if (!response.ok) {
+            throw new Error("failed to fetch todo list. status: ", resp.status);
+        }
+        const data = await response.json();
+        console.log("task added to api:", data.label);
+        const updatedTasks=[
+            ...tasks,
+            { ...newTask, id: data.id }, 
+        ];
+        setTasks(updatedTasks);
+        fetchTasks(setTasks);
+    } catch (error) {
+        console.error("error adding task to api:", error);
+    }
+};
+
+export const deleteTaskFromApi = async (taskId, setTasks) => {
+
+};
